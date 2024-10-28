ae0ba8f0079351146c32a385184824afb7d8cb5c
ddariod <dariod.dazaa09@gmail.com>
2024-10-27 21:01:19 -0500

    tests in the custom hook useFetch

---
+++ tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,25 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+describe("Tests in the hook useFetchGifs", () => {
+  test("should return the initial state", () => {
+    const { result } = renderHook(() => useFetchGifs("One Punch"));
+    const { images, isLoading } = result.current;
+
+    expect(images.length).toBe(0);
+    expect(isLoading).toBeTruthy();
+  });
+
+  test("should return an array of images and the isLoading property in false", async () => {
+    const { result } = renderHook(() => useFetchGifs("One Punch"));
+
+    await waitFor(() =>
+      expect(result.current.images.length).toBeGreaterThan(0)
+    );
+
+    const { images, isLoading } = result.current;
+
+    expect(images.length).toBeGreaterThan(0);
+    expect(isLoading).toBeFalsy();
+  });
+});
